1162da7d932f7d644259953b394e465785c6d904
Wesley Matos <wm4tos.777@gmail.com>
2022-04-16 13:29:42 -0300

    test: write test case to isValidHash method

--- tests/helpers.spec.ts
+++ tests/helpers.spec.ts
@@ -1,5 +1,5 @@
 import { createHash } from 'crypto'
-import { hash } from '../src/helpers'
+import { hash, isValidHash } from '../src/helpers'
 import { chance } from './globals'
 
 describe('helpers', () => {
@@ -8,6 +8,23 @@ describe('helpers', () => {
       const data = chance.string()
 
       expect(hash(data)).toStrictEqual(createHash('sha256').update(data).digest('hex'))
+    })
+  })
+
+  describe('isValidHash', () => {
+    const prefix = String(chance.integer())
+    const quantityOfRepeats = chance.integer({ min: 1, max: 10 })
+
+    it('should return true because hash has right quantity of prefix repeats', () => {
+      const hash = `${prefix.repeat(quantityOfRepeats)}${chance.hash()}`
+
+      expect(isValidHash({ hash, prefix, difficulty: quantityOfRepeats })).toBe(true)
+    })
+
+    it('should return false because hash doesn\'t have the right quantities of repeats', () => {
+      const hash = `${prefix.repeat(quantityOfRepeats - 1)}${chance.hash()}`
+      
+      expect(isValidHash({ hash, prefix, difficulty: quantityOfRepeats })).toBe(false)
     })
   })
 })
