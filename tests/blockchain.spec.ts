4d7b18ea8cf8656414319504bd64f033397b360f
Wesley Matos <wm4tos.777@gmail.com>
2022-04-15 22:53:54 -0300

    test: create global instance to validate blockchain

--- tests/blockchain.spec.ts
+++ tests/blockchain.spec.ts
@@ -1,7 +1,14 @@
 import { Blockchain } from "../src/blockchain"
 
 describe('Blockchain', () => {
+  let blockchain: Blockchain
+
+  beforeEach(() => {
+    blockchain = new Blockchain(4)
+  })
+
   it('generate a valid instance', () => {
-    expect(new Blockchain(4)).toBeInstanceOf(Blockchain)
+    expect(blockchain).toBeDefined()
+    expect(blockchain).toBeInstanceOf(Blockchain)
   })
 })
