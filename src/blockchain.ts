83f3343d38f0e7492abd9535c216d15d4c21cc8e
Wesley Matos <wm4tos.777@gmail.com>
2022-04-15 23:04:39 -0300

    feat: create blockchain private basic methods

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -1,4 +1,36 @@
+import { hash } from "./helpers";
+import { Block, Payload } from "./types";
+
 export class Blockchain {
+  #chain: Block[] = []
+  #prefixPow = '0'
+
   constructor(private readonly difficulty = 4) {
+    this.#chain.push(this.#createFirstBlock())
+  }
+
+  #createFirstBlock(): Block {
+    const payload: Payload = {
+      sequency: 0,
+      timestamp: +new Date(),
+      data: 'First block',
+      oldHash: ''
+    }
+
+    return {
+      header: {
+        nonce: 0,
+        hash: hash(JSON.stringify(payload))
+      },
+      payload
+    }
+  }
+
+  private get lastBlock(): Block {
+    return this.#chain.at(-1) as Block
+  }
+
+  private hashLastBlock(): string {
+    return this.lastBlock.header.hash
   }
 }
