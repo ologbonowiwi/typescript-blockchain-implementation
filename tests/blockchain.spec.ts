07e4446cb8763b407a0ec232f63813135f81d959
Wesley Matos <wm4tos.777@gmail.com>
2022-04-15 23:09:04 -0300

    test: write block creation test

--- tests/blockchain.spec.ts
+++ tests/blockchain.spec.ts
@@ -1,7 +1,14 @@
 import { Blockchain } from "../src/blockchain"
+import { Payload } from "../src/types"
 
 describe('Blockchain', () => {
   let blockchain: Blockchain
+
+  beforeAll(() => {
+    const date = new Date()
+
+    jest.spyOn<any, any>(global, 'Date').mockImplementation(() => date)
+  })
 
   beforeEach(() => {
     blockchain = new Blockchain(4)
@@ -10,5 +17,16 @@ describe('Blockchain', () => {
   it('generate a valid instance', () => {
     expect(blockchain).toBeDefined()
     expect(blockchain).toBeInstanceOf(Blockchain)
+  })
+
+  it('createBlock', () => {
+    const data = 'abcd'
+
+    expect(blockchain.createBlock(data)).toStrictEqual<Payload>({
+      sequency: (blockchain as any).lastBlock.payload.sequency + 1,
+      data,
+      oldHash: (blockchain as any).hashLastBlock(),
+      timestamp: +new Date()
+    })
   })
 })
