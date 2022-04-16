4f1b7a57dc94173a8f98131a6bd7a1a96a888962
Wesley Matos <wm4tos.777@gmail.com>
2022-04-16 13:36:38 -0300

    feat: write mineBlock method

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -1,10 +1,10 @@
-import { hash } from "./helpers";
+import { hash, isValidHash, isValidHash } from "./helpers";
 import { Block, Payload } from "./types";
 
 export class Blockchain {
   #chain: Block[] = []
   #difficulty: number
-  #prefixPow = '0'
+  #prefixProofOfWork = '0'
 
   constructor(difficulty = 4) {
     this.#difficulty = difficulty
@@ -43,6 +43,34 @@ export class Blockchain {
       data,
       oldHash: this.#hashLastBlock(),
       timestamp: +new Date()
+    }
+  }
+
+  mineBlock(block: Payload): Block {
+    let nonce = 0
+    let start = +new Date()
+
+    while(true) {
+      const hashBlock = hash(JSON.stringify(block))
+      const hashProofOfWork = hash(hashBlock + nonce)
+
+      if (isValidHash({ hash: hashProofOfWork, difficulty: this.#difficulty, prefix: this.#prefixProofOfWork })) {
+        const finish = +new Date()
+        const reducedHash = hashBlock.slice(0, 12)
+        const timeMine = (finish - start) / 1000
+
+        console.log(`Block ${block.sequency} mined in ${timeMine} seconds. Hash ${reducedHash} (${nonce} tries)`)
+
+        return {
+          header: {
+            hash: hashBlock,
+            nonce
+          },
+          payload: block
+        }
+      }
+
+      nonce += 1
     }
   }
 }
