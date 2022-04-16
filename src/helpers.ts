1192400bdedbd2a7cd6b1d3f4e8482073ebca5ea
Wesley Matos <wm4tos.777@gmail.com>
2022-04-16 13:29:53 -0300

    feat: implement isValidHash method

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,9 @@
 import { BinaryLike, createHash } from "crypto";
 
 export const hash = (data: BinaryLike): string => createHash('sha256').update(data).digest('hex')
+
+export const isValidHash = ({ hash, difficulty, prefix }: { hash: string, difficulty: number, prefix: string }): boolean => {
+  const check = prefix.repeat(difficulty)
+
+  return hash.startsWith(check)
+}
